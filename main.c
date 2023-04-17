108f6e7848ab2f386ac5161a32c0df4d996c2ab7
Joan-Arnau <130911875+Joan-Arnau@users.noreply.github.com>
2023-04-17 09:49:00 +0200

    bucle

--- main.c
+++ main.c
@@ -12,12 +12,25 @@ int main() {
 
     sopa_t sopa; 
     unsigned int a;
+    char rendirse[8];
        // La sopa de lletres
     printf("Digues la dimensio de la Sopa de LLetres");
     scanf("%d",a);
     sopa.dim = a;
     genera_sopa(&sopa);     // La generem (exemple)
-
+    do
+    {
+        if((se_rendirse(rendirse)))
+        {
+            printf("BEn jugat");
+            // Ver todas las palabras
+        }
+        else
+        {
+            //Comprovar palabra  paramtros
+            //Palabra -- inicio -- direccion
+        }
+    }while(!se_rendirse(rendirse));
     mostra_sopa(&sopa);      // La mostrem per pantalla
     printf("HOLA");
     return 0;
